5c7ee423b5bea9dbb89447b7485a5fb9396a4af2
Hung <datas19961996@gmail.com>
2021-10-28 20:57:26 +0700

    Add project files.

---
+++ DTO/NhanVienDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    class NhanVienDTO
+    {
+        public static string Id_NhanVien { get; set; }
+        public static string Ho_NhanVien { get; set; }
+        public static string Ten_NhanVien { get; set; }
+        public static string Sdt_NhanVien { get; set; }
+
+    }
+}
