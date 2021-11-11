75e81b5c368b21461553c7a7717dcbfa1c992f17
Hung <datas19961996@gmail.com>
2021-11-11 20:59:09 +0700

    Update - Hung

--- BUS/TourBUS.cs
+++ BUS/TourBUS.cs
@@ -47,6 +47,7 @@ namespace BUS
             TourDTO tour = new TourDTO(matour, tentour, dacdiem, maloai);
             return dao.Insert(tour);
             //Hung
+            //Hung
         }
         public bool Delete(string data)
         {
