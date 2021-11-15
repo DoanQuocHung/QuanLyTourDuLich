277a141cb3a36f2fd31e360cc906c25d1025eb8e
Hung <datas19961996@gmail.com>
2021-11-15 15:01:08 +0700

    Update QuanLyDiaDiem_GUI - Hung

---
+++ QuanLyTourDuLich/QuanLyDiaDiem_Sua.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QuanLyTourDuLich
+{
+    public partial class QuanLyDiaDiem_Sua : Form
+    {
+        public QuanLyDiaDiem_Sua()
+        {
+            InitializeComponent();
+        }
+    }
+}
