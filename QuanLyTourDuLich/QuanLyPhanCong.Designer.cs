bd44413d1fe274515edd71cf9bf44219a342f190
Hung <datas19961996@gmail.com>
2021-11-19 17:23:25 +0700

    commit - Hung

--- QuanLyTourDuLich/QuanLyPhanCong.Designer.cs
+++ QuanLyTourDuLich/QuanLyPhanCong.Designer.cs
@@ -29,10 +29,18 @@ namespace QuanLyTourDuLich
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // QuanLyPhanCong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "QuanLyPhanCong";
+            this.Name = "QuanLyPhanCong";
+            this.Text = "Quản Lý Phân Công";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
