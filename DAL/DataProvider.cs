6a30173a307c79bda6f7301fa0aa6c473b669a2d
QuocHung <gamekingdom234@gmail.com>
2021-12-16 15:49:10 +0700

    commit

--- DAL/DataProvider.cs
+++ DAL/DataProvider.cs
@@ -11,9 +11,9 @@ namespace DAL
         public DataProvider() { }
 
         //Học
-        public string connectionString = @"Data Source=DESKTOP-P8VHEBN\HOCDB;Initial Catalog=QUANLYTOURDULICH;USER ID=sa; PASSWORD=songkhoe123";
+        //public string connectionString = @"Data Source=DESKTOP-P8VHEBN\HOCDB;Initial Catalog=QUANLYTOURDULICH;USER ID=sa; PASSWORD=songkhoe123";
         //Hùng
-        //public string connectionString = @"Data Source=DESKTOP-48AE0SM\SQLEXPRESS;Initial Catalog=QUANLYTOURDULICH;Integrated Security=True";
+        public string connectionString = @"Data Source=DESKTOP-48AE0SM\SQLEXPRESS;Initial Catalog=QUANLYTOURDULICH;Integrated Security=True";
         //Doanh
         //public string connectionString = @"Data Source=DESKTOP-TB623K0\MSSQLSERVERY;Initial Catalog=QUANLYTOURDULICH;Integrated Security=True";
         //Nguyen
