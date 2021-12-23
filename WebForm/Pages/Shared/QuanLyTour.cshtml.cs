50ac4b9ec3c935d7d0b2e6e933846d14f3670f32
QuocHung <gamekingdom234@gmail.com>
2021-12-23 10:01:44 +0700

    Update web forms - Hung

---
+++ WebForm/Pages/Shared/QuanLyTour.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebForm.Pages.Pages
+{
+    public class QuanLyTourModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
