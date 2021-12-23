3e8de7e18996d592b8db50068e7c3b8f193de845
thuydoanh <truongthuydoanh99@gmail.com>
2021-12-23 12:28:04 +0700

    Update ChiTietTour_Sua - Doanh

--- QuanLyTourDuLich/QuanLyChiTietTour_Sua.Designer.cs
+++ QuanLyTourDuLich/QuanLyChiTietTour_Sua.Designer.cs
@@ -31,8 +31,8 @@ namespace QuanLyTourDuLich
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QuanLyChiTietTour_Sua));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.ThuTu_num = new System.Windows.Forms.NumericUpDown();
             this.DiaDiem_cb = new System.Windows.Forms.ComboBox();
+            this.txtThuTu = new System.Windows.Forms.TextBox();
             this.IdTour_txt = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -40,56 +40,61 @@ namespace QuanLyTourDuLich
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.ThuTu_num)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.panel1.Controls.Add(this.ThuTu_num);
             this.panel1.Controls.Add(this.DiaDiem_cb);
+            this.panel1.Controls.Add(this.txtThuTu);
             this.panel1.Controls.Add(this.IdTour_txt);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(15, 15);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(548, 309);
+            this.panel1.Size = new System.Drawing.Size(685, 386);
             this.panel1.TabIndex = 1;
-            // 
-            // ThuTu_num
-            // 
-            this.ThuTu_num.Location = new System.Drawing.Point(208, 178);
-            this.ThuTu_num.Name = "ThuTu_num";
-            this.ThuTu_num.Size = new System.Drawing.Size(317, 27);
-            this.ThuTu_num.TabIndex = 8;
             // 
             // DiaDiem_cb
             // 
             this.DiaDiem_cb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.DiaDiem_cb.FormattingEnabled = true;
-            this.DiaDiem_cb.Location = new System.Drawing.Point(208, 116);
+            this.DiaDiem_cb.Location = new System.Drawing.Point(260, 145);
+            this.DiaDiem_cb.Margin = new System.Windows.Forms.Padding(4);
             this.DiaDiem_cb.Name = "DiaDiem_cb";
-            this.DiaDiem_cb.Size = new System.Drawing.Size(317, 28);
+            this.DiaDiem_cb.Size = new System.Drawing.Size(395, 33);
             this.DiaDiem_cb.TabIndex = 7;
+            // 
+            // txtThuTu
+            // 
+            this.txtThuTu.Location = new System.Drawing.Point(260, 225);
+            this.txtThuTu.Margin = new System.Windows.Forms.Padding(4);
+            this.txtThuTu.Name = "txtThuTu";
+            this.txtThuTu.ReadOnly = true;
+            this.txtThuTu.Size = new System.Drawing.Size(395, 31);
+            this.txtThuTu.TabIndex = 6;
             // 
             // IdTour_txt
             // 
-            this.IdTour_txt.Location = new System.Drawing.Point(208, 62);
+            this.IdTour_txt.Location = new System.Drawing.Point(260, 78);
+            this.IdTour_txt.Margin = new System.Windows.Forms.Padding(4);
             this.IdTour_txt.Name = "IdTour_txt";
             this.IdTour_txt.ReadOnly = true;
-            this.IdTour_txt.Size = new System.Drawing.Size(317, 27);
+            this.IdTour_txt.Size = new System.Drawing.Size(395, 31);
             this.IdTour_txt.TabIndex = 6;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(31, 180);
+            this.label3.Location = new System.Drawing.Point(39, 225);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(72, 25);
+            this.label3.Size = new System.Drawing.Size(84, 29);
             this.label3.TabIndex = 4;
             this.label3.Text = "Thứ tự";
             // 
@@ -97,9 +102,10 @@ namespace QuanLyTourDuLich
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(31, 119);
+            this.label2.Location = new System.Drawing.Point(39, 149);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(93, 25);
+            this.label2.Size = new System.Drawing.Size(108, 29);
             this.label2.TabIndex = 3;
             this.label2.Text = "Địa điểm";
             // 
@@ -107,31 +113,34 @@ namespace QuanLyTourDuLich
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(31, 61);
+            this.label1.Location = new System.Drawing.Point(39, 76);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 25);
+            this.label1.Size = new System.Drawing.Size(96, 29);
             this.label1.TabIndex = 2;
             this.label1.Text = "ID Tour";
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(412, 253);
+            this.button2.Location = new System.Drawing.Point(515, 316);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(113, 43);
+            this.button2.Size = new System.Drawing.Size(141, 54);
             this.button2.TabIndex = 1;
             this.button2.Text = "Hủy";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.buttonHuy_Click);
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(282, 253);
+            this.button1.Location = new System.Drawing.Point(352, 316);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(109, 43);
+            this.button1.Size = new System.Drawing.Size(136, 54);
             this.button1.TabIndex = 0;
             this.button1.Text = "      Sửa";
             this.button1.UseVisualStyleBackColor = true;
@@ -139,17 +148,17 @@ namespace QuanLyTourDuLich
             // 
             // QuanLyChiTietTour_Sua
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.ClientSize = new System.Drawing.Size(570, 333);
+            this.ClientSize = new System.Drawing.Size(712, 416);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "QuanLyChiTietTour_Sua";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sửa chi tiết Tour";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.ThuTu_num)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -162,8 +171,8 @@ namespace QuanLyTourDuLich
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.NumericUpDown ThuTu_num;
         private System.Windows.Forms.ComboBox DiaDiem_cb;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtThuTu;
     }
 }
