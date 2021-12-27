ef29933ee02785a63ee4a56d818e4d75a3fb35e9
kidakai12 <dzehox@gmail.com>
2021-12-27 15:13:48 +0700

    Update Xoa, Sua, Them DiaDiem- HocVT

--- WebForm/Pages/Shared/QuanLyDiaDiem_Sua.cshtml.cs
+++ WebForm/Pages/Shared/QuanLyDiaDiem_Sua.cshtml.cs
@@ -4,13 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using DTO;
+using BUS;
 namespace WebForm.Pages.Shared
 {
     public class QuanLyDiaDiem_SuaModel : PageModel
     {
+        [BindProperty]
+        public DiaDiemDTO diadiem { set; get; }
         public void OnGet()
         {
+        }
+        public IActionResult OnPost()
+        {
+            if (new DiaDiemBUS().Update(diadiem))
+            {
+                return Redirect("/Shared/QuanLyDiaDiem");
+            }
+            return Page();
         }
     }
 }
