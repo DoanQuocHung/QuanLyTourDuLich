8123f4640819f9598eaac65581231761c46d5075
kidakai12 <dzehox@gmail.com>
2021-12-29 21:17:30 +0700

    Update ChitietTour - HocVT

--- WebForm/Pages/Shared/QuanLyChiTietTour.cshtml.cs
+++ WebForm/Pages/Shared/QuanLyChiTietTour.cshtml.cs
@@ -4,13 +4,44 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using BUS;
+using DTO;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 namespace WebForm.Pages.Shared
 {
     public class QuanLyChiTietTourModel : PageModel
     {
+        [BindProperty]
+        public ChiTietTourDTO chitiet { get; set; }
         public void OnGet()
         {
+
+        }
+
+        public IActionResult OnPostEdit()
+        {
+            var str = JsonConvert.SerializeObject(chitiet);
+            HttpContext.Session.SetString("editchitiettour",str);
+            return RedirectToPage("/Shared/QuanLyChiTietTour_Sua");
+        }
+
+        public IActionResult OnPostDelete()
+        {
+            try
+            {
+                if (new ChiTietTourBUS().Delete(chitiet.Id_Tour,chitiet.Id_DiaDiem))
+                {
+                    HttpContext.Session.SetString("xoachitiettour", "thanhcong");
+                    return Page();
+                }
+            }
+            catch (Exception e1)
+            {
+                HttpContext.Session.SetString("xoachitiettour", "khongthanhcong");
+                return Page();
+            }
+            return Page();
         }
     }
 }
--- WebForm/Pages/Shared/QuanLyChiTietTour_Them.cshtml.cs
+++ WebForm/Pages/Shared/QuanLyChiTietTour_Them.cshtml.cs
@@ -4,13 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using BUS;
+using DTO;
 namespace WebForm.Pages.Shared
 {
     public class QuanLyChiTietTour_ThemModel : PageModel
     {
+        [BindProperty]
+        public ChiTietTourDTO tour { get; set; }
+        public List<DiaDiemDTO> listdiadiem { get; set; }
         public void OnGet()
         {
+            listdiadiem = new DiaDiemBUS().List();
+        }
+
+        public IActionResult OnPostAdd()
+        {
+            if (new ChiTietTourBUS().Insert(tour))
+            {
+                return Redirect("/Shared/QuanLyChiTietTour");
+            }
+            return Page();
         }
     }
 }
