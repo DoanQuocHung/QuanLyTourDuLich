97ef4225567d5fb198de5f1139c37fc0393ae74e
kidakai12 <dzehox@gmail.com>
2021-12-30 18:38:25 +0700

    Update ThongKe WebForm-HocVT

--- WebForm/Pages/Shared/QuanLyChiTietTour.cshtml.cs
+++ WebForm/Pages/Shared/QuanLyChiTietTour.cshtml.cs
@@ -14,6 +14,24 @@ namespace WebForm.Pages.Shared
     {
         [BindProperty]
         public ChiTietTourDTO chitiet { get; set; }
+
+        [BindProperty]
+        public string khachsan { get; set; }
+        [BindProperty]
+        public string anuong { get; set; }
+        [BindProperty]
+        public string phuongtien { get; set; }
+        [BindProperty]
+        public string doanhso { get; set; }
+        [BindProperty]
+        public string sodoan { get; set; }
+        [BindProperty]
+        public string tong { get; set; }
+
+        [BindProperty]
+        public string search1 { get; set; }
+        [BindProperty]
+        public string search2 { get; set; }
         public void OnGet()
         {
 
@@ -41,6 +59,35 @@ namespace WebForm.Pages.Shared
                 HttpContext.Session.SetString("xoachitiettour", "khongthanhcong");
                 return Page();
             }
+            return Page();
+        }
+        public IActionResult OnPostTong()
+        {
+            string idtour = HttpContext.Session.GetString("chitiettour");
+            List<DoanDuLichDTO> list = new DoanDuLichBUS().List().FindAll(x => x.Id_Tour.Equals(idtour));
+
+            var parsedDate1 = DateTime.ParseExact(search1, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+            search1 = parsedDate1.ToString("dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+            var parsedDate2 = DateTime.ParseExact(search2, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+            search2 = parsedDate2.ToString("dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+
+            ThongKeBUS bus = new ThongKeBUS();
+            khachsan = bus.chiphikhachsan(list, search1, search2).ToString();
+            anuong = bus.chiphianuong(list, search1, search2).ToString();
+            phuongtien = bus.chiphiphuongtien(list, search1, search2).ToString();
+            sodoan = bus.sodoan(list, search1, search2).ToString();
+            doanhso = bus.doanhso(list, search1, search2).ToString();
+            tong = bus.tongtatca(list, search1, search2).ToString();
+
+            HttpContext.Session.SetString("thongketimkiem", "thanhcong");
+            HttpContext.Session.SetString("khachsan", khachsan);
+            HttpContext.Session.SetString("anuong", anuong);
+            HttpContext.Session.SetString("phuongtien", phuongtien);
+            HttpContext.Session.SetString("sodoan", sodoan);
+            HttpContext.Session.SetString("doanhso", doanhso);
+            HttpContext.Session.SetString("tong", tong);
+
+
             return Page();
         }
     }
