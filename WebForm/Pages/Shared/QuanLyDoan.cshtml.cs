c380cf276ba33c34803b57387b60f3cef2a15671
kidakai12 <dzehox@gmail.com>
2021-12-30 09:24:16 +0700

    Update Doan Them - HocVT

--- WebForm/Pages/Shared/QuanLyDoan.cshtml.cs
+++ WebForm/Pages/Shared/QuanLyDoan.cshtml.cs
@@ -4,13 +4,49 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using BUS;
+using DTO;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 namespace WebForm.Pages.Shared
 {
     public class QuanLyDoanModel : PageModel
     {
+        [BindProperty]
+        public DoanDuLichDTO doan { set; get; }
         public void OnGet()
         {
+
+        }
+        public IActionResult OnPostEdit()
+        {
+            string str = JsonConvert.SerializeObject(doan);
+            HttpContext.Session.SetString("editdoan", str);
+            return RedirectToPage("/Shared/QuanLyDoan_Sua");
+        }
+        public IActionResult OnPostDelete()
+        {
+            try
+            {
+                if (new DoanDuLichBUS().Delete(doan.Id_Doan))
+                {
+                    HttpContext.Session.SetString("xoadoan", "thanhcong");
+                    return Page();
+                }
+            }
+            catch (Exception e)
+            {
+                HttpContext.Session.SetString("xoadoan", "khongthanhcong");
+                return Page();
+            }
+            return Page();
+        }
+        public IActionResult OnPostDetail()
+        {
+            string gia = new DoanDuLichBUS().getGiaTour(doan.Ngaykhoihanh, doan.Id_Tour);
+            HttpContext.Session.SetString("chitietdoan-giatour", gia);
+            HttpContext.Session.SetString("chitietdoan", doan.Id_Doan);
+            return RedirectToPage("/Shared/QuanLyChiTietDoan");
         }
     }
 }
--- WebForm/Pages/Shared/QuanLyDoan_Them.cshtml.cs
+++ WebForm/Pages/Shared/QuanLyDoan_Them.cshtml.cs
@@ -4,13 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using BUS;
+using DTO;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 namespace WebForm.Pages.Shared
 {
     public class QuanLyDoan_ThemModel : PageModel
     {
+        [BindProperty]
+        public DoanDuLichDTO doan { set; get; }
+        public List<TourDTO> listtour { get; set; }
         public void OnGet()
         {
+            listtour = new TourBUS().List().FindAll(x => x.Tinh_Trang == 1);
+        }
+        public IActionResult OnPostAdd()
+        {
+            var parsedDate1 = DateTime.ParseExact(doan.Ngaykhoihanh, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+            doan.Ngaykhoihanh = parsedDate1.ToString("dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+            var parsedDate2 = DateTime.ParseExact(doan.Ngayketthuc, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+            doan.Ngayketthuc = parsedDate2.ToString("dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+
+            doan.Doanhthu = 0;
+            if (new DoanDuLichBUS().Insert(doan))
+            {
+                return Redirect("/Shared/QuanLyDoan");
+            }
+            return Page();
         }
     }
 }
