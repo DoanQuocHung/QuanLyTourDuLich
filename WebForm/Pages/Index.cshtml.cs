1e0d3b6c8316aa03c963091947f4fb1238f0d964
kidakai12 <dzehox@gmail.com>
2021-12-30 19:20:54 +0700

    Update WebForm- HocVT

--- WebForm/Pages/Index.cshtml.cs
+++ WebForm/Pages/Index.cshtml.cs
@@ -5,8 +5,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using BUS;
 using DTO;
+using BUS;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 namespace WebForm.Pages
 {
     public class IndexModel : PageModel
@@ -18,9 +20,39 @@ namespace WebForm.Pages
             _logger = logger;
         }
 
+        [BindProperty]
+        public TourDTO tour { set; get; }
         public void OnGet()
         {
-            
+
+        }
+        public IActionResult OnPostEdit()
+        {
+            string str = JsonConvert.SerializeObject(tour);
+            HttpContext.Session.SetString("edittour", str);
+            return RedirectToPage("/Shared/QuanLyTour_Sua");
+        }
+        public IActionResult OnPostDelete()
+        {
+            try
+            {
+                if (new TourBUS().Delete(tour.Id_Tour))
+                {
+                    HttpContext.Session.SetString("xoatour", "thanhcong");
+                    return Page();
+                }
+            }
+            catch (Exception e1)
+            {
+                HttpContext.Session.SetString("xoatour", "khongthanhcong");
+                return Page();
+            }
+            return Page();
+        }
+        public IActionResult OnPostDetail()
+        {
+            HttpContext.Session.SetString("chitiettour", tour.Id_Tour);
+            return RedirectToPage("/Shared/QuanLyChiTietTour");
         }
     }
 }
