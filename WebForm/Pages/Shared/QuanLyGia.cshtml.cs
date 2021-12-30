99180a883bb01ae10c9ec193bd80731f8621f7c3
QuocHung <gamekingdom234@gmail.com>
2021-12-30 17:22:51 +0700

    Update QL Gia - Hung

--- WebForm/Pages/Shared/QuanLyGia.cshtml.cs
+++ WebForm/Pages/Shared/QuanLyGia.cshtml.cs
@@ -4,13 +4,43 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using BUS;
+using DTO;
 
 namespace WebForm.Pages.Shared
 {
     public class QuanLyGiaModel : PageModel
     {
+        [BindProperty]
+        public GiaDTO gia { set; get; }
         public void OnGet()
         {
+
+        }
+        public IActionResult OnPostEdit()
+        {
+            string str = JsonConvert.SerializeObject(gia);
+            HttpContext.Session.SetString("editgia", str);
+            return RedirectToPage("/Shared/QuanLyGia_Sua");
+        }
+        public IActionResult OnPostDelete()
+        {
+            try
+            {
+                if (new DiaDiemBUS().Delete(gia.Id_Gia))
+                {
+                    HttpContext.Session.SetString("xoagia", "thanhcong");
+                    return Page();
+                }
+            }
+            catch (Exception e1)
+            {
+                HttpContext.Session.SetString("xoagia", "khongthanhcong");
+                return Page();
+            }
+            return Page();
         }
     }
 }
--- WebForm/Pages/Shared/QuanLyGia_Them.cshtml.cs
+++ WebForm/Pages/Shared/QuanLyGia_Them.cshtml.cs
@@ -4,13 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BUS;
+using DTO;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace WebForm.Pages.Shared
 {
     public class QuanLyGia_ThemModel : PageModel
     {
+        [BindProperty]
+        public GiaDTO gia { set; get; }
         public void OnGet()
         {
+        }
+        public IActionResult OnPost()
+        {
+            if (new GiaBUS().Insert(new GiaDTO(
+                gia.Id_Gia,
+                gia.Id_Tour, 
+                gia.Gia, 
+                gia.Thoigianbatdau, 
+                gia.Thoigianketthuc)))
+            {
+                return Redirect("/Shared/QuanLyGia");
+            }
+            return Page();
         }
     }
 }
