3a85b59b87f7af22806399f27242a7bfbbd6514f
kidakai12 <dzehox@gmail.com>
2021-12-30 17:11:44 +0700

    Update chitietDoan - HocVT

---
+++ WebForm/Pages/Shared/QuanLyChiPhi_Sua.cshtml.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using BUS;
+using DTO;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+namespace WebForm.Pages.Shared
+{
+    public class QuanLyChiPhi_SuaModel : PageModel
+    {
+        [BindProperty]
+        public ChiPhiDTO chiphi { set; get; }
+
+        [BindProperty]
+        public string loai { set; get; }
+
+        [BindProperty]
+        public string phi { set; get; }
+        public List<LoaiChiPhiDTO> loaichiphi { set; get; }
+        public void OnGet()
+        {
+            loaichiphi = new LoaiChiPhiBUS().List();
+            var str = HttpContext.Session.GetString("editchiphidoan");
+            var obj = JsonConvert.DeserializeObject<ChiPhiDTO>(str);
+            loai = obj.Id_LoaiChiPhi;
+        }
+
+        public IActionResult OnPostEdit()
+        {
+            string tmp = chiphi.Id_LoaiChiPhi;
+            chiphi.Id_LoaiChiPhi = loai;
+            if (new ChiPhiBUS().Update(chiphi,tmp))
+            {
+                long value2 = chiphi.Gia - long.Parse(phi);
+                new DoanDuLichBUS().UpdateDoanhThu(chiphi.Id_Doan, value2);
+                return Redirect("/Shared/QuanLyChiTietDoan");
+            }
+            return Page();
+        }
+    }
+}
--- WebForm/Pages/Shared/QuanLyChiTietDoan.cshtml.cs
+++ WebForm/Pages/Shared/QuanLyChiTietDoan.cshtml.cs
@@ -4,13 +4,49 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using BUS;
+using DTO;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 namespace WebForm.Pages.Shared
 {
     public class QuanLyChiTietDoanModel : PageModel
     {
+        [BindProperty]
+        public ChiTietDoanDTO chitiet { set; get; }
+
+        [BindProperty]
+        public ChiPhiDTO chiphi { set; get; }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPostDelete()
+        {
+            string gia = HttpContext.Session.GetString("chitietdoan-giatour");
+            if (new ChiTietDoanBUS().Delete(chitiet.Id_Doan, chitiet.Id_Khach))
+            {
+                new DoanDuLichBUS().UpdateDoanhThu(chitiet.Id_Doan, long.Parse(gia));
+                return Redirect("/Shared/QuanLyChiTietDoan");
+            }
+            return Page();
+        }
+
+        public IActionResult OnPostDelete2()
+        {
+            if (new ChiPhiBUS().Delete(chiphi.Id_LoaiChiPhi, chiphi.Id_Doan))
+            {
+                new DoanDuLichBUS().UpdateDoanhThu(chiphi.Id_Doan, chiphi.Gia);
+                return Redirect("/Shared/QuanLyChiTietDoan");
+            }
+            return Page();
+        }
+
+        public IActionResult OnPostEdit2()
+        {
+            string str = JsonConvert.SerializeObject(chiphi);
+            HttpContext.Session.SetString("editchiphidoan", str);
+            return RedirectToPage("/Shared/QuanLyChiPhi_Sua");
         }
     }
 }
